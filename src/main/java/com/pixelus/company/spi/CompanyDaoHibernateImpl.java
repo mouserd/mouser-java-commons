b5b2bae899cf7d5a254bfc934fcb8b73b80dd947
David Mouser <david.mouser@gmai.com>
2012-03-13 17:50:37 +1100

    #5 Added copyright notice to all .java files.

--- src/main/java/com/pixelus/company/spi/CompanyDaoHibernateImpl.java
+++ src/main/java/com/pixelus/company/spi/CompanyDaoHibernateImpl.java
@@ -1,3 +1,9 @@
+/*
+ * This source code is copyright (c) 2012 Pixelus Consulting Pty Ltd.  All rights reserved.
+ *
+ * Should you wish to use or enquire about any of the content contained please contact
+ * David Mouser (david.mouser@gmail.com).
+ */
 package com.pixelus.company.spi;
 
 import org.hibernate.SessionFactory;
