3569ee09bcf6a3014150be811e74b0a687dc0115
David Mouser <david.mouser@gmai.com>
2012-03-16 23:07:23 +1100

    Updated copyright.

--- integration/src/test/java/com/pixelus/repository/spi/CompanyRepositoryHibernateImplTest.java
+++ integration/src/test/java/com/pixelus/repository/spi/CompanyRepositoryHibernateImplTest.java
@@ -1,8 +1,9 @@
 /*
- * This source code is copyright (c) 2012 Pixelus Consulting Pty Ltd.  All rights reserved.
+ * This source code is copyright (c) 2012 Pixelus Consulting Pty Ltd.  All
+ * rights reserved.
  *
- * Should you wish to use or enquire about any of the content contained please contact
- * David Mouser (david.mouser@gmail.com).
+ * Should you wish to use or enquire about any of the content contained please
+ * contact David Mouser (david.mouser@gmail.com).
  */
 
 package com.pixelus.repository.spi;
