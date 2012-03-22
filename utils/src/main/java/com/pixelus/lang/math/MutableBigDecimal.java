390b1a930b36c6dfc7cba569a86a3a6a47e9aae8
David Mouser <david.mouser@gmai.com>
2012-03-22 21:57:12 +1100

    * Created a new 'utilities' module and in it the start of a mutable BigDecimal implementation. * Fixed checkstyle from not actually being produced unless site was run due to missing maven jxr plugin. * Added ability to supress checkstyle errors for individual classes/checks via the checkstyle-supressions.xml file.

---
+++ utils/src/main/java/com/pixelus/lang/math/MutableBigDecimal.java
@@ -0,0 +1,85 @@
+/*
+ * This source code is copyright (c) 2012 Pixelus Consulting Pty Ltd.  All
+ * rights reserved.
+ *
+ * Should you wish to use or enquire about any of the content contained please 
+ * contact David Mouser (david.mouser@gmail.com).
+ */
+
+package com.pixelus.lang.math;
+
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+
+/*
+* @author David Mouser
+*/
+public class MutableBigDecimal {
+
+    private BigDecimal bigDecimal;
+
+    public MutableBigDecimal(BigDecimal bigDecimal) {
+
+        this.bigDecimal = bigDecimal;
+    }
+
+    public MutableBigDecimal() {
+
+        bigDecimal = BigDecimal.ZERO;
+    }
+
+    public MutableBigDecimal(int i) {
+
+        bigDecimal = new BigDecimal(i);
+    }
+
+    public MutableBigDecimal(double d) {
+
+        bigDecimal = new BigDecimal(d);
+    }
+
+    public MutableBigDecimal(long l) {
+
+        bigDecimal = new BigDecimal(l);
+    }
+
+    public int intValue() {
+
+        return bigDecimal.intValue();
+    }
+
+    public double doubleValue() {
+
+        return bigDecimal.doubleValue();
+    }
+
+    public Long longValue() {
+
+        return bigDecimal.longValue();
+    }
+
+    public void add(BigDecimal augend) {
+
+        bigDecimal = bigDecimal.add(augend);
+    }
+
+    public void subtract(BigDecimal augend) {
+
+        bigDecimal = bigDecimal.subtract(augend);
+    }
+
+    public void divide(BigDecimal augend) {
+
+        bigDecimal = bigDecimal.divide(augend);
+    }
+
+    public void multiply(BigDecimal augend) {
+
+        bigDecimal = bigDecimal.multiply(augend);
+    }
+
+    public void divide(BigDecimal augend, int precision, RoundingMode roundingMode) {
+
+        bigDecimal = bigDecimal.divide(augend, precision, roundingMode);
+    }
+}
---
+++ utils/src/test/java/com/pixelus/lang/math/MutableBigDecimalTest.java
@@ -0,0 +1,101 @@
+/*
+ * This source code is copyright (c) 2012 Pixelus Consulting Pty Ltd.  All
+ * rights reserved.
+ *
+ * Should you wish to use or enquire about any of the content contained please 
+ * contact David Mouser (david.mouser@gmail.com).
+ */
+
+package com.pixelus.lang.math;
+
+import org.junit.Test;
+
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.Is.is;
+
+/*
+* @author David Mouser
+*/
+public class MutableBigDecimalTest {
+
+    @Test
+    public void testDefaultConstructor() {
+
+        assertThat(new MutableBigDecimal().intValue(), is(0));
+    }
+
+    @Test
+    public void testBigDecimalConstructor() {
+
+        assertThat(new MutableBigDecimal(new BigDecimal(10)).intValue(), is(10));
+    }
+
+    @Test
+    public void testIntConstructor() {
+
+        assertThat(new MutableBigDecimal(1).intValue(), is(1));
+    }
+
+    @Test
+    public void testDoubleConstructor() {
+
+        assertThat(new MutableBigDecimal(1.0).doubleValue(), is(1.0));
+    }
+
+    @Test
+    public void testLongConstructor() {
+
+        assertThat(new MutableBigDecimal(1L).longValue(), is(1L));
+    }
+
+    @Test
+    public void testAdd() {
+
+        MutableBigDecimal bigDecimal = new MutableBigDecimal(10);
+        bigDecimal.add(new BigDecimal(10));
+
+        assertThat(bigDecimal.intValue(), is(20));
+    }
+
+    @Test
+    public void testSubtract() {
+
+        MutableBigDecimal bigDecimal = new MutableBigDecimal(20);
+        bigDecimal.subtract(new BigDecimal(10));
+
+        assertThat(bigDecimal.intValue(), is(10));
+    }
+
+    @Test
+    public void testDivide() {
+
+        MutableBigDecimal bigDecimal = new MutableBigDecimal(20);
+        bigDecimal.divide(new BigDecimal(4));
+
+        assertThat(bigDecimal.intValue(), is(5));
+
+        bigDecimal.divide(new BigDecimal(2));
+        assertThat(bigDecimal.doubleValue(), is(2.5));
+    }
+
+    @Test
+    public void testDivideWithRounding() {
+
+        MutableBigDecimal bigDecimal = new MutableBigDecimal(10);
+        bigDecimal.divide(new BigDecimal(3), 2, RoundingMode.HALF_UP);
+
+        assertThat(bigDecimal.doubleValue(), is(3.33));
+    }
+
+    @Test
+    public void testMultiply() {
+
+        MutableBigDecimal bigDecimal = new MutableBigDecimal(10);
+        bigDecimal.multiply(new BigDecimal(10));
+
+        assertThat(bigDecimal.intValue(), is(100));
+    }
+}
