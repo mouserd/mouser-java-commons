db4fd1ab44e6b271c7a7c986c1f5813e290939e9
David Mouser <david.mouser@gmail.com>
2012-09-20 09:32:27 +1000

    Added basic jasmine support to web POM and related jasmine and sinon javascript libraries.

---
+++ web/src/test/javascript/lib/sinon/jasmine-sinon.js
@@ -0,0 +1,52 @@
+/*
+ * This source code is copyright (c) 2012 Pixelus Consulting Pty Ltd.  All
+ * rights reserved.
+ *
+ * Should you wish to use or enquire about any of the content contained please
+ * contact David Mouser (david.mouser@gmail.com).
+ */
+
+(function (global) {
+
+    var spyMatchers = "called calledOnce calledTwice calledThrice calledBefore calledAfter calledOn alwaysCalledOn calledWith alwaysCalledWith calledWithExactly alwaysCalledWithExactly".split(" "),
+        i = spyMatchers.length,
+        spyMatcherHash = {},
+        unusualMatchers = {
+            "returned":"toHaveReturned",
+            "alwaysReturned":"toHaveAlwaysReturned",
+            "threw":"toHaveThrown",
+            "alwaysThrew":"toHaveAlwaysThrown"
+        },
+
+        getMatcherFunction = function (sinonName) {
+            return function () {
+                var sinonProperty = this.actual[sinonName];
+                return (typeof sinonProperty === 'function') ? sinonProperty.apply(this.actual, arguments) : sinonProperty;
+            };
+        };
+
+    while (i--) {
+        var sinonName = spyMatchers[i],
+            matcherName = "toHaveBeen" + sinonName.charAt(0).toUpperCase() + sinonName.slice(1);
+
+        spyMatcherHash[matcherName] = getMatcherFunction(sinonName);
+    }
+    ;
+
+    for (var j in unusualMatchers) {
+        spyMatcherHash[unusualMatchers[j]] = getMatcherFunction(j);
+    }
+
+    global.sinonJasmine = {
+        getMatchers:function () {
+            return spyMatcherHash;
+        }
+    };
+
+})(window);
+
+beforeEach(function () {
+
+    this.addMatchers(sinonJasmine.getMatchers());
+
+});
---
+++ web/src/test/javascript/test_spec.js
@@ -0,0 +1,14 @@
+/*
+ * This source code is copyright (c) 2012 Pixelus Consulting Pty Ltd.  All
+ * rights reserved.
+ *
+ * Should you wish to use or enquire about any of the content contained please
+ * contact David Mouser (david.mouser@gmail.com).
+ */
+
+describe("something should do something", function () {
+    it("should do something", function () {
+        expect(false).toBeFalsy();
+    });
+});
+
